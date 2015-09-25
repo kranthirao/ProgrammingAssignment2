6521dd989fb90387e2ba9cd42327789a374fd512
Kranthi <kranthirao@gmail.com>
2015-09-24 22:26:19 -0400

    Test2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Test commit.
-
-## Write a short comment describing this function
-
+## This function takes a matrix and calculates the inverse of the given matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        #set <- function(y) {
+        #       x <<- y
+        #       m <<- NULL
+        #}
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(get = get,setinv = setinv,getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This function Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # print ("in cS")
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("Fetching cached data")
+                return(m)
+        }
+        data <- x$get()
+        print ("first time in cacheSolve...")
+        print (data)
+        m <- solve(data, ...)
+        print ("Storing result set using setinv...")
+        x$setinv(m)
+        m
 }
