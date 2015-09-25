629422a6b1acb8c3e233b023bf668d826079c620
Kranthi <kranthirao@gmail.com>
2015-09-24 22:58:06 -0400

    Assigment2 - Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,33 @@
 ## This function takes a matrix and calculates the inverse of the given matrix
 makeCacheMatrix <- function(x = matrix()) {
+        # Initialze matrix 
         m <- NULL
-        #set <- function(y) {
-        #       x <<- y
-        #       m <<- NULL
-        #}
+        set <- function(y) {
+               x <<- y
+               m <<- NULL
+        }
         get <- function() x
         setinv <- function(inv) m <<- inv
         getinv <- function() m
-        list(get = get,setinv = setinv,getinv = getinv)
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
-
 
 ## This function Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        # print ("in cS")
+        # Get matrix from env
         m <- x$getinv()
+        # if exists/not NULL, fetch from cache (return matrix).
         if(!is.null(m)) {
                 message("Fetching cached data")
                 return(m)
         }
+        # First time , assign given matrix 
         data <- x$get()
-        print ("first time in cacheSolve...")
-        print (data)
+        # Compute inverse of matrix
         m <- solve(data, ...)
-        print ("Storing result set using setinv...")
+        # Store result set using setinv
         x$setinv(m)
         m
 }
